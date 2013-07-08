d72851d34ed558aa93b54908499821d7c3bd3453
Scott Hendrickson <scott@drskippy.net>
2013-07-08 17:04:16 -0600

    Documented code

--- python/request-to-arduino.py
+++ python/request-to-arduino.py
@@ -1,16 +1,64 @@
 #!/usr/bin/env python
+#  WWC Meetup
+#     Gnip and Arduino Fun!
+#     2013-07-10
+#
+###########################################
+#
+# Make sure you have pyserial and requests packages install and working for your python installation
+# Pyserial http://pyserial.sourceforge.net/
+# Requests http://docs.python-requests.org/en/latest/
+#
+###########################################
+
 import serial
 import requests
 import json
 import sys
 import time
 
+###########################################
+# Temporary Gnip interface to real tim social data
 TOP_URL = "http://shendrickson3.gnip.com:8090/redr8r/v1/top.json"
-# colon delimited list of items to look for in the redis store
+
+# Example output
+#  {
+#          timestamp: 1373322884,
+#          version: "v1",
+#          topkeys: {
+#              blue: 862,
+#              justin: 1270,
+#              egypt: 716,
+#              sun: 1118,
+#              one: 383,
+#              black: 2428,
+#              bieber: 1323,
+#              amp: 407,
+#              red: 1623,
+#              obama: 690
+#              }
+#          }
+
 CNT_URL = "http://shendrickson3.gnip.com:8090/redr8r/v1/%s/count.json"
+
+# Example output
+# {
+#         keys: {
+#             black: {
+#                 count: 17628
+#                 }
+#             },
+#         timestamp: 1373324445,
+#         version: "v1"
+#         }
+###########################################
+# ls the contents of your /dev folder.  Look for something
+# like below. Change the string to match yours.
 
 ser = serial.Serial('/dev/tty.usbmodemfd121', 9600)
 #ser = serial.Serial('/dev/tty.usbmodemfa131', 9600)
+
+###########################################
 
 colors = ["red", "blue", "yellow", "orange"]
 
